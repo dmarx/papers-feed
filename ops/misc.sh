02e56ad6ee83138074c42bc6e4e55062654ea1f6
David Marx <david.marx84@gmail.com>
2025-01-03 22:32:04 -0800

    trigger op: remove cruft

--- ops/misc.sh
+++ ops/misc.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+git rm -r papers-feed-src
+git rm -r src
+git rm -r tests
+git rm .github/workflows/test-python.yml
+git rm .github/workflows/update-submodule.yml
+git rm .github/workflows/ops-remove-files.yml
