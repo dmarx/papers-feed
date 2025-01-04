7099b76da5c635546c6c0be356156f493fb9da77
dmarx <dmarx@users.noreply.github.com>
2025-01-04 06:33:40 +0000

    chore: executed miscellaneous operation

--- ops/misc.sh
+++ ops/misc.sh
@@ -1,7 +1,1 @@
 #!/bin/bash
-git rm -r papers-feed-src
-git rm -r src
-git rm -r tests
-git rm .github/workflows/test-python.yml
-git rm .github/workflows/update-submodule.yml
-git rm .github/workflows/ops-remove-files.yml
