ed74adb2d119acf7ab96d795c0e2924fdc8a1bb0
David Marx <david.marx84@gmail.com>
2025-02-09 13:41:55 -0800

    Update misc.sh

--- ops/misc.sh
+++ ops/misc.sh
@@ -3,3 +3,4 @@ pip install llamero
 python scripts/process_pdf.py flush_old_conversions --tag=""
 python scripts/process_pdf.py flush_old_conversions --tag="grobid"
 python scripts/process_pdf.py generate_missing_conversions
+
