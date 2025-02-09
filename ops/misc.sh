cd2cbd77561bde1d6d7e78c8f39bf7cad56377fd
David Marx <david.marx84@gmail.com>
2025-02-09 13:38:43 -0800

    yolo grobid markdown conversions

--- ops/misc.sh
+++ ops/misc.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+pip install llamero
+python scripts/process_pdf.py flush_old_conversions --tag=""
+python scripts/process_pdf.py flush_old_conversions --tag="grobid"
+python scripts/process_pdf.py generate_missing_conversions
