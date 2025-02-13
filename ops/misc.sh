f27b7440a02c9a4283fd50e363f48da3c4180dd7
David Marx <david.marx84@gmail.com>
2025-02-12 16:37:33 -0800

    Update misc.sh

--- ops/misc.sh
+++ ops/misc.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-pip install llamero lxml
-python scripts/process_pdf.py flush_old_conversions --tag=""
-python scripts/process_pdf.py flush_old_conversions --tag="grobid"
-python scripts/process_pdf.py generate_missing_conversions
+
+find data/papers -name "*_grobid.md" -o -name "*_grobid.tei.xml" | while read file; do
+    dir=$(dirname "$file")
+    id=$(basename "$dir")
+    ext="${file##*.}"
+    type=$(if [[ $file == *_grobid.md ]]; then echo "markdown-grobid"; else echo "tei-xml-grobid"; fi)
+    mkdir -p "$dir/features/$type"
+    mv "$file" "$dir/features/$type/$id.$ext"
+done
