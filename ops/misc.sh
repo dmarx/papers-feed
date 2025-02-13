507a827e4018549749f384cddfb6d468c2110ab4
dmarx <1466881+dmarx@users.noreply.github.com>
2025-02-13 00:38:48 +0000

    chore: executed miscellaneous operation

--- ops/misc.sh
+++ ops/misc.sh
@@ -1,10 +1,1 @@
 #!/bin/bash
-
-find data/papers -name "*_grobid.md" -o -name "*_grobid.tei.xml" | while read file; do
-    dir=$(dirname "$file")
-    id=$(basename "$dir")
-    ext="${file##*.}"
-    type=$(if [[ $file == *_grobid.md ]]; then echo "markdown-grobid"; else echo "tei-xml-grobid"; fi)
-    mkdir -p "$dir/features/$type"
-    mv "$file" "$dir/features/$type/$id.$ext"
-done
