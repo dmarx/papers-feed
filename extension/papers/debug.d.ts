33e947b02b7f156e46a0bf371b4bcdd3a26cea1f
David Marx <david.marx84@gmail.com>
2025-03-08 17:02:58 -0800

    Update debug.d.ts

--- extension/papers/debug.d.ts
+++ extension/papers/debug.d.ts
@@ -2,12 +2,20 @@
 import { GitHubStoreClient } from 'gh-store-client';
 import { PaperManager } from './manager';
 
+// In extension/papers/debug.d.ts, update the global interface
 declare global {
-    const __DEBUG__: {
-        paperManager: PaperManager | null;
-        getGithubClient: () => GitHubStoreClient | undefined;
-        getCurrentPaper: () => any;
-        getCurrentSession: () => any;
-        getConfig: () => any;
+    interface Window {
+        __DEBUG__: {
+            paperManager: PaperManager | null;
+            getGithubClient: () => GitHubStoreClient | undefined;
+            getCurrentPaper: () => any;
+            getCurrentSession: () => any;
+            getConfig: () => any;
+            enhancedServices?: {
+                urlDetectionService: any;
+                getPluginState: () => any;
+                handleUrl: (url: string) => Promise<any>;
+            };
+        };
     }
 }
