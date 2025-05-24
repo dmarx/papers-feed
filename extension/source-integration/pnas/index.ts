5c89087426793f164e6debdce817a09509177d36
David Marx <david.marx84@gmail.com>
2025-05-24 14:19:11 -0700

    minimal pnas integration

---
+++ extension/source-integration/pnas/index.ts
@@ -0,0 +1,32 @@
+// extension/source-integration/pnas/index.ts
+// pnas.org integration with custom metadata extractor
+
+import { BaseSourceIntegration } from '../base-source';
+import { PaperMetadata } from '../../papers/types';
+import { MetadataExtractor, ExtractedMetadata } from '../metadata-extractor';
+import { loguru } from '../../utils/logger';
+
+const logger = loguru.getLogger('nature-integration');
+
+
+export class PnasIntegration extends BaseSourceIntegration {
+  readonly id = 'pnas';
+  readonly name = 'PNAS'; 
+
+  readonly urlPatterns = [
+   // /nature\.com\/articles\/([^?]+)/,
+    /pnas\.org\/doi\/10\.\d+\/pnas\.(\d+)/
+  ];
+
+  // Content script matches  
+  readonly contentScriptMatches = [
+    "*://*.pnas.org/doi/*"
+  ];
+
+  // upstream BaseSourceIntegration.extractPaperId should default to this behavior when able
+  extractPaperId(url: string): string | null {
+    const match = url.match(this.urlPatterns[0]);
+    return match ? match[1] : null;
+  }
+
+export const pnasIntegration = new PnasIntegration();
