0db40c63a590296677919e3af0f8604585e59342
David Marx <david.marx84@gmail.com>
2025-05-24 18:31:38 -0700

    added a bunch of domains to tracking

--- extension/source-integration/misc/index.ts
+++ extension/source-integration/misc/index.ts
@@ -24,12 +24,17 @@ export class MiscIntegration extends BaseSourceIntegration {
     "researchgate.net/publication/",
     "psycnet.apa.org/record/",
     "biorxiv.org/content/",
+    "osf.io/preprints/",
+    "frontiersin.org/journals/",
+    "jstor.org/",
+    "proceedings.mlr.press/",
+    "journals.plos.org/plosone/article",
+    "ieeexplore.ieee.org/document/",
   ];
 
   canHandleUrl(url: string): boolean {
     return this.contentScriptMatches.some(pattern => url.includes(pattern));
   }
-  
 }
 
 export const miscIntegration = new MiscIntegration();
