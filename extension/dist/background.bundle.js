3d25831b1349d45e223ba3d4cdc9e079d87128e9
dmarx <1466881+dmarx@users.noreply.github.com>
2025-06-05 14:08:16 +0000

    chore: Update extension bundles

--- extension/dist/background.bundle.js
+++ extension/dist/background.bundle.js
@@ -718,8 +718,8 @@ const logger$5 = loguru.getLogger('icon-utils');
 const ICONS = {
     default: {
         path: {
-            "16": "icons/bookmark/red/favicon-16x16.png",
-            "32": "icons/bookmark/red/favicon-32x32.png",
+            "16": "icons/bookmark/red/apple-touch-icon.png",
+            "32": "icons/bookmark/red/apple-touch-icon.png",
             "48": "icons/bookmark/red/apple-touch-icon.png",
             "128": "icons/bookmark/red/apple-touch-icon.png"
         },
@@ -727,8 +727,8 @@ const ICONS = {
     },
     detected: {
         path: {
-            "16": "icons/bookmark/blue/favicon-16x16.png",
-            "32": "icons/bookmark/blue/favicon-32x32.png",
+            "16": "icons/bookmark/blue/apple-touch-icon.png",
+            "32": "icons/bookmark/blue/apple-touch-icon.png",
             "48": "icons/bookmark/blue/apple-touch-icon.png",
             "128": "icons/bookmark/blue/apple-touch-icon.png"
         },
@@ -736,10 +736,10 @@ const ICONS = {
     },
     tracked: {
         path: {
-            "16": "icons/bookmark/green/favicon-16x16.png",
-            "32": "icons/bookmark/green/favicon-32x32.png",
-            "48": "icons/bookmark/green/apple-touch-icon.png",
-            "128": "icons/bookmark/green/apple-touch-icon.png"
+            "16": "icons/bookmark/forest/apple-touch-icon.png",
+            "32": "icons/bookmark/forest/apple-touch-icon.png",
+            "48": "icons/bookmark/forest/apple-touch-icon.png",
+            "128": "icons/bookmark/forest/apple-touch-icon.png"
         },
         title: "Paper Tracked - Academic Paper Tracker"
     }
